2d34ba5a76fd3a3f2d493e168b02e67eab7d2ac3
rohits-web03 <rohit.dey302003@gmail.com>
2023-10-10 02:40:49 +0530

    Created routes folder to keep Home.jsx,Projects.jsx and Contributors.jsx

---
+++ src/routes/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='py-28 text-center text-4xl'>Home</div>
+  )
+}
---
+++ src/routes/Projects.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Projects() {
+  return (
+    <div className='py-28 text-center text-4xl'>Projects</div>
+  )
+}
