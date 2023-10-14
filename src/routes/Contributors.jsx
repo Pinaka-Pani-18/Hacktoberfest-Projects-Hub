3593923f0815c3d2e96e956ec43d15444183c832
Sh4dab <shadabali2k2@gmail.com>
2023-10-14 16:57:36 +0530

    Enhancement:Contributor Page

--- src/routes/Contributors.jsx
+++ src/routes/Contributors.jsx
@@ -42,26 +42,34 @@ export const Contributors = () => {
     }, []);
 
     return (
-        <div className="min-h-screen">
-            <h1 className="text-center text-5xl pt-12">Contributors</h1>
-            <div className="flex flex-wrap justify-center p-5 gap-8">
+        <div className="min-h-screen mt-20">
+            <h1 className="text-center text-7xl pt-12 font-bold text-[#1f2937]">Contributors</h1>
+            <div className="flex flex-wrap justify-center gap-8 p-20">
                 {error ? (
                     <h1 className="text-2xl">{error}</h1>
                 ) : (
                     contributors.map((contributor) => (
                         <div
                             onClick={() => window.open(contributor.html_url)}
-                            className="cursor-pointer transform transition-transform hover:scale-110 shadow-sm bg-white rounded-lg p-5"
+                            className="cursor-pointer shadow-lg bg-white rounded-lg p-5"
                             key={contributor.id}
                             style={{ width: "200px" }}
                         >
                             <img
                                 src={contributor.avatar_url}
                                 alt="Contributor Avatar"
-                                className="w-36 h-36 object-cover mx-auto rounded-full"
+                                className="w-36 h-36 object-cover mx-auto rounded-full transform transition-transform hover:scale-110 duration-300  hover:shadow-blue-900 shadow-lg"
                             />
-                            <p className="text-center font-semibold mt-4">
-                                {contributor.login}
+                            <p className="text-center mt-6">
+                                <p className=" font-bold text-lg">{contributor.login}</p>
+                                {contributor.login === owner ?
+                                    <h1 className="text-gray-700 font-semibold">Project Admin</h1>
+                                    :
+                                    <h1 className="text-gray-700 font-semibold">Contributor</h1>
+                                }
+                                <p className="text-sm"> {contributor.contributions}{" "}
+                                    {contributor.contributions === 1 ? "Commit" : "Commits"}
+                                </p>
                             </p>
                         </div>
                     ))
